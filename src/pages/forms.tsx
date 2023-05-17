1e47caff2c0dcd223bdb0a6dfa5c9c13450dea67
saeed.eyvazy <saeed.eyvazy@gmail.com>
2023-05-17 21:20:12 +0430

    Clean code :recycle:

--- src/pages/forms.tsx
+++ src/pages/forms.tsx
@@ -1,4 +1,4 @@
-import { mdiAccount, mdiBallotOutline, mdiMail, mdiSearchWeb } from '@mdi/js'
+import { mdiAccount, mdiMail, mdiSearchWeb } from '@mdi/js'
 import { Field, Form, Formik } from 'formik'
 import Head from 'next/head'
 import { ReactElement, useState } from 'react'
@@ -6,13 +6,13 @@ import BaseButton from '../components/BaseButton'
 import BaseDivider from '../components/BaseDivider'
 import CardBox from '../components/CardBox'
 import FormField from '../components/FormField'
-import LayoutAuthenticated from '../layouts/Authenticated'
 import SectionMain from '../components/SectionMain'
 import SectionTitleLineWithButton from '../components/SectionTitleLineWithButton'
-import { getPageTitle, iaxios } from '../config'
 import { TeacherTable } from '../components/TeacherTable'
-import { useTeacher } from '../hooks/useTeacher'
+import { getPageTitle, iaxios } from '../config'
 import { API_SPECIFIC_TEACHER_SEARCH } from '../constants'
+import { useTeacher } from '../hooks/useTeacher'
+import LayoutAuthenticated from '../layouts/Authenticated'
 
 const FormsPage = () => {
   const { data, error, isLoading, totalTeacherLength } = useTeacher()
@@ -83,94 +83,11 @@ const FormsPage = () => {
                   <TeacherTable clients={searchResult} isLoading={searchLoading} error={error} />
                 }
               </CardBox>
-              {/* <FormField
-                label="With help line and labelFor"
-                labelFor="phone"
-                help="Help line comes here"
-              >
-                <Field name="phone" placeholder="Phone" id="phone" />
-              </FormField>
 
-              <FormField label="Dropdown" labelFor="color">
-                <Field name="color" id="color" component="select">
-                  <option value="red">Red</option>
-                  <option value="green">Green</option>
-                  <option value="blue">Blue</option>
-                </Field>
-              </FormField> */}
-
-
-              {/* <FormField label="Textarea" hasTextareaHeight>
-                <Field name="textarea" as="textarea" placeholder="Your text here" />
-              </FormField>
-
-              <BaseDivider />
-
-              <BaseButtons>
-                <BaseButton type="submit" color="info" label="Submit" />
-                <BaseButton type="reset" color="info" outline label="Reset" />
-              </BaseButtons> */}
             </Form>
           </Formik>
         </CardBox>
       </SectionMain>
-
-      {/* <SectionTitle>Custom elements</SectionTitle>
-
-      <SectionMain>
-        <CardBox>
-          <Formik
-            initialValues={{ checkboxes: ['lorem'], switches: ['lorem'], radio: 'lorem' }}
-            onSubmit={() => null}
-          >
-            <Form>
-              <FormField label="Checkbox">
-                <FormCheckRadioGroup>
-                  <FormCheckRadio type="checkbox" label="Lorem">
-                    <Field type="checkbox" name="checkboxes" value="lorem" />
-                  </FormCheckRadio>
-                  <FormCheckRadio type="checkbox" label="Ipsum">
-                    <Field type="checkbox" name="checkboxes" value="ipsum" />
-                  </FormCheckRadio>
-                  <FormCheckRadio type="checkbox" label="Dolore">
-                    <Field type="checkbox" name="checkboxes" value="dolore" />
-                  </FormCheckRadio>
-                </FormCheckRadioGroup>
-              </FormField>
-
-              <BaseDivider />
-
-              <FormField label="Radio">
-                <FormCheckRadioGroup>
-                  <FormCheckRadio type="radio" label="Lorem">
-                    <Field type="radio" name="radio" value="lorem" />
-                  </FormCheckRadio>
-                  <FormCheckRadio type="radio" label="Ipsum">
-                    <Field type="radio" name="radio" value="ipsum" />
-                  </FormCheckRadio>
-                </FormCheckRadioGroup>
-              </FormField>
-
-              <BaseDivider />
-
-              <FormField label="Switch">
-                <FormCheckRadioGroup>
-                  <FormCheckRadio type="switch" label="Lorem">
-                    <Field type="checkbox" name="switches" value="lorem" />
-                  </FormCheckRadio>
-                  <FormCheckRadio type="switch" label="Ipsum">
-                    <Field type="checkbox" name="switches" value="ipsum" />
-                  </FormCheckRadio>
-                </FormCheckRadioGroup>
-              </FormField>
-            </Form>
-          </Formik>
-          <BaseDivider />
-          <FormField>
-            <FormFilePicker label="Upload" color="info" icon={mdiUpload} />
-          </FormField>
-        </CardBox>
-      </SectionMain> */}
     </>
   )
 }

6e603926eb4eb890ff5b3c23d6ce41a8802976d9
saeed.eyvazy <saeed.eyvazy@gmail.com>
2023-05-17 15:53:24 +0430

    Change userType required in form validation

--- src/validation/form.js
+++ src/validation/form.js
@@ -1,7 +1,7 @@
 import * as Yup from 'yup'
 
 export const instLoginFormValidation = Yup.object().shape({
-    userType: Yup.string().required('Required'),
+    userType: Yup.string().required('نوع کاربری را انتخاب کنید'),
     username: Yup.string().required('نام کاربری خود را وارد کنید'),
     password: Yup.string().required('رمز عبور خود را وارد کنید')
 })

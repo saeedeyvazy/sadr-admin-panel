b5589d64964029dc64bf48848bb612ff554d52ea
saeed.eyvazy <saeed.eyvazy@gmail.com>
2023-05-18 12:43:17 +0430

    Clean code :recycle:

--- src/hooks/useAxios.ts
+++ src/hooks/useAxios.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { iaxios } from '../config'
 
 const useAxios = ({ url, method, body = null, headers = null }) => {

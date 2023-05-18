5442001988042f0d7ca5f2132f20173cb1dae9c3
saeed.eyvazy <saeed.eyvazy@gmail.com>
2023-05-18 12:44:17 +0430

    Add validation schema for adding manager

--- src/validation/form.js
+++ src/validation/form.js
@@ -5,3 +5,9 @@ export const instLoginFormValidation = Yup.object().shape({
     username: Yup.string().required('نام کاربری خود را وارد کنید'),
     password: Yup.string().required('رمز عبور خود را وارد کنید')
 })
+
+export const changeManagerValidation = Yup.object().shape({
+    nationalCode: Yup.string().required('کد ملی جباریست'),
+    name: Yup.string().required('نام اجباریست و توسط سیستم پر میشود'),
+    startDate: Yup.string().required('تاریخ شروع به کار اجباریست')
+})

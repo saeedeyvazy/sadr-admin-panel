9326e927d3aef190c8b6ab825626ddc94fa78f06
saeed.eyvazy <saeed.eyvazy@gmail.com>
2023-05-19 20:59:16 +0430

    Remove some items in menuNavBar

--- src/menuNavBar.ts
+++ src/menuNavBar.ts
@@ -68,20 +68,6 @@ const menuNavBar: MenuNavBarItem[] = [
     isToggleLightDark: true,
   },
   {
-    icon: mdiGithub,
-    label: 'GitHub',
-    isDesktopNoLabel: true,
-    href: 'https://github.com/justboil/admin-one-react-tailwind',
-    target: '_blank',
-  },
-  {
-    icon: mdiVuejs,
-    label: 'Vue version',
-    isDesktopNoLabel: true,
-    href: 'https://github.com/justboil/admin-one-vue-tailwind',
-    target: '_blank',
-  },
-  {
     icon: mdiLogout,
     label: 'Log out',
     isDesktopNoLabel: true,

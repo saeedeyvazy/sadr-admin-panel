6db0dc182ebbc44fcaf17e0e2f198c116c83ad1a
saeed.eyvazy <saeed.eyvazy@gmail.com>
2023-05-19 19:38:31 +0430

    Change DirectorBoardCardBox component style :lipstick:

--- src/components/institution/DirectorBoardCardBox.tsx
+++ src/components/institution/DirectorBoardCardBox.tsx
@@ -1,9 +1,10 @@
+import { mdiDelete } from '@mdi/js'
 import { labels } from '../../constants/labels'
 import { Member } from '../../interfaces'
+import { Avatar } from '../Avatar'
 import BaseButton from '../BaseButton'
 import CardBox from '../CardBox'
 import PillTag from '../PillTag'
-import UserAvatar from '../UserAvatar'
 
 type Props = {
   member: Member,
@@ -17,7 +18,7 @@ export const DirectorBoardCardBox = (props: Props) => {
     <CardBox className="mb-6 last:mb-0 shadow-md border border-gray-200 cursor-pointer hover:bg-gray-100 transition duration-500">
       <div className="flex flex-col md:flex-row items-center justify-between">
         <div className="flex flex-col md:flex-row items-center justify-start mb-6 md:mb-0">
-          <UserAvatar className="w-16 h-16 md:mr-6 mb-6 md:mb-0" username={props.member.pic} />
+          <Avatar className="w-16 h-16 md:mr-6 mb-6 md:mb-0 rounded-full border  bg-gray-200" avatar={props.member.pic} username={props.member.id.toString()} />
           <div className="text-center md:text-right overflow-hidden mx-2 space-y-1">
             <h4 className="text-xl text-ellipsis">{`${props.member.fname} ${props.member.lname}`}</h4>
             <p className="text-gray-500 text-sm whitespace-nowrap dark:text-slate-300">
@@ -27,7 +28,7 @@ export const DirectorBoardCardBox = (props: Props) => {
             <PillTag className='whitespace-nowrap flex-row-reverse' color="success" label={props.member.ozviat} />
           </div>
         </div>
-        <BaseButton onClick={props.deleteItem} label={labels.delete} color='danger' type='button' />
+        <BaseButton icon={mdiDelete} onClick={props.deleteItem} label={labels.delete} color='danger' type='button' />
       </div>
     </CardBox>
   )

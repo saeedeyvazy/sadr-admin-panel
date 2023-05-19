5a30695870ffe55d1a28e0ebbd6447134a795b02
saeed.eyvazy <saeed.eyvazy@gmail.com>
2023-05-19 19:56:44 +0430

    Fix DirectorBoardCardBox component :bug:

--- src/components/institution/DirectorBoardCardBox.tsx
+++ src/components/institution/DirectorBoardCardBox.tsx
@@ -18,7 +18,7 @@ export const DirectorBoardCardBox = (props: Props) => {
     <CardBox className="mb-6 last:mb-0 shadow-md border border-gray-200 cursor-pointer hover:bg-gray-100 transition duration-500">
       <div className="flex flex-col md:flex-row items-center justify-between">
         <div className="flex flex-col md:flex-row items-center justify-start mb-6 md:mb-0">
-          <Avatar className="w-16 h-16 md:mr-6 mb-6 md:mb-0 rounded-full border  bg-gray-200" avatar={props.member.pic} username={props.member.id.toString()} />
+          <Avatar className="w-16 h-16 md:mr-6 mb-6 md:mb-0 rounded-full border  bg-gray-200" avatar={props.member.pic} username={props.member.id?.toString()} />
           <div className="text-center md:text-right overflow-hidden mx-2 space-y-1">
             <h4 className="text-xl text-ellipsis">{`${props.member.fname} ${props.member.lname}`}</h4>
             <p className="text-gray-500 text-sm whitespace-nowrap dark:text-slate-300">
